27365338f51616b0ddce2cd99ff96ff12675319c
palacetamon098 <158472954+palacetamon098@users.noreply.github.com>
2024-02-03 17:46:36 +0700

    Update uqyap.lua

--- uqyap.lua
+++ uqyap.lua
@@ -1,3 +1,110 @@
-_G.U = "https://discord.com/api/webhooks/1202094127957676072/jO5iM_nL8tKKj9lYbuLvUmlPYpKwHW_6J9HY9tH1GYHITw-NHeoH9OMI8p3NsewDajkd"
-_G.M = "https://discord.com/api/webhooks/1202587163213176833/uzG7F3uwqq2qLOjZwm35c7bmvF6ZzJReXXGyYOnmVfs7iVdNJPZ-JSAKT3zp-VWhlcZM"
-_G.FM = "https://discord.com/api/webhooks/1202588124878741614/jWZgWDQVDJXTqPOnnA97gzyII-I_QtxhqF_sFFHabLaU36oi__jkdwaL26Ottdd3DQPl"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/palacetamon098/gwamrbswhpouf/main/tshpwqklytuirxfzvb'))()
+
+           if game:GetService("Workspace").Map:FindFirstChild("MysticIsland") then
+local ExecutorUsing = is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or secure_load and "Sentinel" or KRNL_LOADED and "Krnl" or SONA_LOADED and "Sona" or "Shit exploit g"
+                    local HttpService = game:GetService("HttpService")
+                    local Data =
+                        {
+                            ["embeds"]= {
+                               {
+
+                                    ["fields"]= {
+
+                                        {
+                                            ["name"]= "Mirage island",
+                                            ["value"]= "```found```",
+                                            ["inline"]= true
+                                        },
+                                        {
+                                            ["name"]= "join server",
+                                            ["value"]= 'game:GetService("TeleportService"):TeleportToPlaceInstance('..game.PlaceId..', "'..game.JobId..'", game.Players.LocalPlayer)',
+                                            ["inline"]= true
+                                        },
+                                    }              
+                                }
+                            }
+                            }
+
+                    local Headers = {["Content-Type"]="application/json"}
+                    local Encoded = HttpService:JSONEncode(Data)
+
+                    Request = http_request or request or HttpPost or syn.request
+                    local Final1 = {Url = _G.M , Body = Encoded, Method = "POST", Headers = Headers}
+
+                    Request(Final1)
+           end
+                if game.PlaceId == 7449423635 then 
+                if game:GetService("Lighting").Sky.MoonTextureId=="http://www.roblox.com/asset/?id=9709149052" then
+local ExecutorUsing = is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or secure_load and "Sentinel" or KRNL_LOADED and "Krnl" or SONA_LOADED and "Sona" or "Shit exploit g"
+                    local HttpService = game:GetService("HttpService")
+                    local Data =
+                        {
+                            ["embeds"]= {
+                               {
+
+                                    ["fields"]= {
+
+                                        {
+                                            ["name"]= "moon : 3/4 (🌘)",
+                                            ["value"]= "who want do trial?",
+                                            ["inline"]= true
+                                        },
+                                        {
+                                            ["name"]= "join server",
+                                            ["value"]= 'game:GetService("TeleportService"):TeleportToPlaceInstance('..game.PlaceId..', "'..game.JobId..'", game.Players.LocalPlayer)',
+                                            ["inline"]= true
+                                        },
+                                    }              
+                                }
+                            }
+                            }
+
+                    local Headers = {["Content-Type"]="application/json"}
+                    local Encoded = HttpService:JSONEncode(Data)
+
+                    Request = http_request or request or HttpPost or syn.request
+                    local Final1 = {Url = _G.FM , Body = Encoded, Method = "POST", Headers = Headers}
+
+                    Request(Final1)
+                end
+                end
+                local ExecutorUsing = is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Trigon" or secure_load and "Sentinel" or KRNL_LOADED and "Krnl" or SONA_LOADED and "Sona" or "Fluxus" or
+"Fluxus"
+                    local HttpService = game:GetService("HttpService")
+                     local Data =
+                        {
+                            ["embeds"]= {
+                                {            
+                                    ["title"]= "Profile";
+                                    ["url"]= "https://www.roblox.com/users/"..game.Players.LocalPlayer.UserId;
+                                    ["description"]= "```"..game.Players.LocalPlayer.DisplayName.." ("..game.Players.LocalPlayer.Name..") ```";
+                                    ["color"]= tonumber(cc00cc);
+                                     ["thumbnail"] = {["url"] = "https://cdn.discordapp.com/attachments/1181972023539421204/1202094832580034651/K.png?ex=65cc350d&is=65b9c00d&hm=de633a27401a66cc752a46835f7b35d169ad90130bb1e39af62c5c21e9ae3458&"};
+                                    
+                                    ["fields"]= {
+                                        {
+                                            ["name"]= "excutor📑",
+                                            ["value"]= "```"..ExecutorUsing.."```",
+                                            ["inline"]= true
+                                        },
+                                        {
+                                            ["name"]= "age🗓",
+                                            ["value"]= "```"..game.Players.LocalPlayer.AccountAge.." Days```",
+                                            ["inline"]= true
+                                        },
+                                        {
+                                            ["name"]= "ty for using📜",
+                                            ["value"]= "```Mbm Hub!!!```",
+                                            ["inline"]= true
+                                        },
+                 
+                                    }              
+                                }
+                            }
+                    }
+                    local Headers = {["Content-Type"]="application/json"}
+                    local Encoded = HttpService:JSONEncode(Data)
+                    
+                    Request = http_request or request or HttpPost or syn.request
+                    local Final = {Url = _G.U , Body = Encoded, Method = "POST", Headers = Headers}
+                    Request(Final)
