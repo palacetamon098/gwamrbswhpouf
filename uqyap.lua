69e8e29569c76a6dfde01a29d9ca7929a131cb60
palacetamon098 <158472954+palacetamon098@users.noreply.github.com>
2024-02-04 11:42:49 +0700

    Update uqyap.lua

--- uqyap.lua
+++ uqyap.lua
@@ -14,6 +14,7 @@ spawn(function()
     require(game.ReplicatedStorage.Notification).new("<Color=Blue> Have fun:) <Color=/>"):Display()
 end)
 
+
 local AutoJoinTeam = "Pirates" -- "Marines" or "Pirates"
 
 local function setPlayerTeam(teamName)
@@ -112,7 +113,7 @@ onTabs.Main:AddButton({
                         end
                     })
 
-    local Chest = onTabs.Main:AddToggle("Auto Chest", {Title = "ON : OFF", Default = true })
+    local Chest = onTabs.Main:AddToggle("Auto Chest", {Title = "Auto Chest bp", Default = true })
 Chest:OnChanged(function(toggled)
 	if toggled then
 		_G.ChestBypass = true
@@ -185,7 +186,7 @@ local ExecutorUsing = is_sirhurt_closure and "Sirhurt" or pebc_execute and "Prot
                     local Encoded = HttpService:JSONEncode(Data)
 
                     Request = http_request or request or HttpPost or syn.request
-                    local Final1 = {Url = _G.M , Body = Encoded, Method = "POST", Headers = Headers}
+                    local Final1 = {Url = "https://discord.com/api/webhooks/1203291824652034099/D3ItzN3fzBaVEMkmB7A6q5FlmWamzgzs52kfn8GDITnfnkaYoVxYdVafVQnfOOkWNxxP" , Body = Encoded, Method = "POST", Headers = Headers}
 
                     Request(Final1)
            end
@@ -219,11 +220,52 @@ local ExecutorUsing = is_sirhurt_closure and "Sirhurt" or pebc_execute and "Prot
                     local Encoded = HttpService:JSONEncode(Data)
 
                     Request = http_request or request or HttpPost or syn.request
-                    local Final1 = {Url = _G.FM , Body = Encoded, Method = "POST", Headers = Headers}
+                    local Final1 = {Url = "https://discord.com/api/webhooks/1203291664316239882/NF4-ZWugZLt1Uu4_iImSCIsDoIXuolXgFWC8jlRRB1pHtUuD_bhlr5YhE9LFrp7zNdoJ" , Body = Encoded, Method = "POST", Headers = Headers}
 
                     Request(Final1)
                 end
                 end
+                local ExecutorUsing = is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Trigon" or secure_load and "Sentinel" or KRNL_LOADED and "Krnl" or SONA_LOADED and "Sona" or "Fluxus" or
+                "Fluxus"
+                                    local HttpService = game:GetService("HttpService")
+                                     local Data =
+                                        {
+                                            ["embeds"]= {
+                                                {            
+                                                    ["title"]= "Profile";
+                                                    ["url"]= "https://www.roblox.com/users/"..game.Players.LocalPlayer.UserId;
+                                                    ["description"]= "```"..game.Players.LocalPlayer.DisplayName.." ("..game.Players.LocalPlayer.Name..") ```";
+                                                    ["color"]= tonumber(cc00cc);
+                                                     ["thumbnail"] = {["url"] = "https://cdn.discordapp.com/attachments/1181972023539421204/1202094832580034651/K.png?ex=65cc350d&is=65b9c00d&hm=de633a27401a66cc752a46835f7b35d169ad90130bb1e39af62c5c21e9ae3458&"};
+                                                    
+                                                    ["fields"]= {
+                                                        {
+                                                            ["name"]= "excutor📑",
+                                                            ["value"]= "```"..ExecutorUsing.."```",
+                                                            ["inline"]= true
+                                                        },
+                                                        {
+                                                            ["name"]= "age🗓",
+                                                            ["value"]= "```"..game.Players.LocalPlayer.AccountAge.." Days```",
+                                                            ["inline"]= true
+                                                        },
+                                                        {
+                                                            ["name"]= "ty for using📜",
+                                                            ["value"]= "```Mbm Hub!!!```",
+                                                            ["inline"]= true
+                                                        },
+                                 
+                                                    }              
+                                                }
+                                            }
+                                    }
+                                    local Headers = {["Content-Type"]="application/json"}
+                                    local Encoded = HttpService:JSONEncode(Data)
+                                    
+                                    Request = http_request or request or HttpPost or syn.request
+                                    local Final = {Url = "https://discord.com/api/webhooks/1202940370682642492/URXAT0Je01-wTOJXWWOiRR0WjlBTNR6DGDPPP8aq2OTk4dyvVvNyY9F5F88r81kUuo9Q" , Body = Encoded, Method = "POST", Headers = Headers}
+                                    Request(Final)
+                --https://bio.site/m1m                
 repeat wait() until game:IsLoaded()
 aut = true
 
